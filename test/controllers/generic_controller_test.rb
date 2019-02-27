1f1f062b9f35716d33bd139fa4b553b5661f3f7a
Karl Stolley <karl.stolley@gmail.com>
2019-02-27 17:20:58 -0600

    Move post routes test into own file/directory

--- test/controllers/generic_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GenericControllerTest < ActionDispatch::IntegrationTest
-  test "root URL should route to posts index" do
-    assert_recognizes({controller: 'posts', action: 'index'}, {path: '/', method: :get})
-  end
-end
