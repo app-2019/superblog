44a0084080a7d4b0bb7ee6757709e45038ef6241
Karl Stolley <karl.stolley@gmail.com>
2019-03-04 17:49:35 -0600

    Require a title on each post

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,10 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "post must have a title" do
+    @post = posts(:missing_title)
+    refute @post.save
+    @post.title = 'OK, You Have a Title'
+    assert @post.save
+  end
 end
