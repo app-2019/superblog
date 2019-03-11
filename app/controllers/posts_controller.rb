3c3a795087efb5d685f6e4a2b3894827d77ab165
Karl Stolley <karl.stolley@gmail.com>
2019-03-11 17:44:33 -0500

    Update controller and view to handle author_name

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :slug, :content, :author_id)
+      params.require(:post).permit(:title, :slug, :content, :author_name)
     end
 end
