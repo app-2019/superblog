821dddb3f37238abf456859ef3517c0cc9ccf3bb
Karl Stolley <karl.stolley@gmail.com>
2019-03-11 17:33:46 -0500

    Add author_name getter and setter methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
 class Post < ApplicationRecord
   validates :title, presence: true
   belongs_to :author
+
+  def author_name=(name)
+    self.author = Author.find_or_create_by(name: name)
+  end
+
+  def author_name
+    self.author ? self.author.name : nil
+  end
+
+
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -10,4 +10,16 @@ class PostTest < ActiveSupport::TestCase
     @post.title = 'OK, You Have a Title'
     assert @post.save
   end
+
+  test "post responds to author_name getter" do
+    @post = posts(:good_post)
+    assert_equal @post.author_name, posts(:good_post).author.name
+  end
+
+  test "post responses to author_name setter" do
+    @post = posts(:good_post)
+    @post.author_name = authors(:hank).name
+    @post.save
+    assert_equal @post.author_name, authors(:hank).name
+  end
 end
