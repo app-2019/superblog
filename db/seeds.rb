c8b5f631ff7a6e0c774071da0dda7b29f2819946
Karl Stolley <karl.stolley@gmail.com>
2019-03-11 17:33:15 -0500

    Add seed data (for shame)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@author = Author.create(name: 'Karl Stolley')
+@post = Post.create(title: 'Seeded Post', slug: 'seeded-post', content: 'Seeded post content.', author: @author)
