9e7bf8d6aec4e7188925a8bcef64a8c80c1b40bd
Karl Stolley <karl.stolley@gmail.com>
2019-04-02 16:10:52 -0500

    Run migration for additional auth fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_164535) do
+ActiveRecord::Schema.define(version: 2019_04_02_210515) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "login", default: ""
+    t.string "uid", default: ""
+    t.string "email", default: ""
   end
 
   create_table "posts", force: :cascade do |t|
