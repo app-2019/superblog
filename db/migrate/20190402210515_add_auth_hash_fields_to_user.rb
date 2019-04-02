cf8019439bfc134604f16619196d243defd9ad0f
Karl Stolley <karl.stolley@gmail.com>
2019-04-02 16:09:39 -0500

    Add login, uid, and email fields to Author

--- db/migrate/20190402210515_add_auth_hash_fields_to_user.rb
+++ db/migrate/20190402210515_add_auth_hash_fields_to_user.rb
@@ -1,4 +1,7 @@
 class AddAuthHashFieldsToUser < ActiveRecord::Migration[5.2]
   def change
+    add_column :authors, :login, :string, default: ''
+    add_column :authors, :uid, :string, default: ''
+    add_column :authors, :email, :string, default: ''
   end
 end
