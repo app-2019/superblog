d22d3ec271499335f027fde8f827a9ad5765bfb3
Karl Stolley <karl.stolley@gmail.com>
2019-04-03 18:08:41 -0500

    Add uid, nickname, and email fields to Author

--- db/migrate/20190403230445_add_auth_hash_fields_to_authors.rb
+++ db/migrate/20190403230445_add_auth_hash_fields_to_authors.rb
@@ -1,4 +1,7 @@
 class AddAuthHashFieldsToAuthors < ActiveRecord::Migration[5.2]
   def change
+    add_column :authors, :uid, :string, default: ''
+    add_column :authors, :nickname, :string, default: ''
+    add_column :authors, :email, :string, default: ''
   end
 end
