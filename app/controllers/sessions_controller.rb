307096ea3b51737cddd63f2f735944222752e4c5
Karl Stolley <karl.stolley@gmail.com>
2019-04-02 21:30:55 -0500

    Find or create Author from OAuth hash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,11 @@
 class SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
   def create
-    @provider_response = request.env['omniauth.auth']
+    auth_hash = request.env['omniauth.auth']
+    @author = Author.find_or_create_by(uid: auth_hash['uid']) do | author|
+      author.login = auth_hash['info']['nickname']
+      author.name = auth_hash['info']['name']
+      author.email = auth_hash['info']['email']
+    end
   end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,25 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get sessions_create_url
-    assert_response :success
+
+  setup do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      provider: 'github',
+      uid: '123456',
+      info: {
+        name: 'karlstolley',
+        nickname: 'Karl Stolley',
+        email: 'karl@example.com'
+      }
+    })
+  end
+
+  test "authorization should create a new author" do
+    # post '/auth/github/callback', params: { uid: '123456' }
+    assert_difference('Author.count') do
+      post '/auth/github/callback', params: OmniAuth.config.mock_auth[:github]
+    end
   end
 
 end
