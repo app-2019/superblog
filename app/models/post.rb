bf38da6fdc06fc0dbdc64426b1c0862c0ac4d633
Karl Stolley <karl.stolley@gmail.com>
2019-04-10 17:28:20 -0500

    Attach one photo to the Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   belongs_to :author
   has_and_belongs_to_many :tags
 
+  has_one_attached :photo
+
   def author_name=(name)
     self.author = Author.find_or_create_by(name: name)
   end
