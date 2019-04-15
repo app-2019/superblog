71ae7393e5901a763dcfe88c08cc332f2d334f39
Karl Stolley <karl.stolley@gmail.com>
2019-04-15 11:11:45 -0500

    Add a test on author creation

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get sessions_create_url
-    assert_response :success
+
+  test "authorization should create a new author" do
+    # post '/auth/github/callback', params: { uid: '123456' }
+    assert_difference('Author.count') do
+      post '/auth/github/callback', params: OmniAuth.config.add_mock(:github, {uid: 112233})
+    end
   end
 
 end
