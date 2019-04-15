4349caddd9e857119a692ff461e82d9c7bbcc05f
Karl Stolley <karl.stolley@gmail.com>
2019-04-15 17:36:19 -0500

    Move OmniAuth setup to test helper

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,18 +2,6 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
 
-  setup do
-    OmniAuth.config.test_mode = true
-    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
-      uid: authors(:karl).uid,
-      info: {
-        name: authors(:karl).name,
-        nickname: authors(:karl).nickname,
-        email: authors(:karl).email
-      }
-    })
-  end
-
   test "should get create" do
     post '/auth/github/callback', params: OmniAuth.config.mock_auth[:github]
     assert_redirected_to posts_url
