12d8621e37286d671fe1deee8654dbbeb9b047ec
Karl Stolley <karl.stolley@gmail.com>
2019-04-15 17:28:27 -0500

    Configure OmniAuth for testing and create mock hash

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,6 +1,19 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      uid: authors(:karl).uid,
+      info: {
+        name: authors(:karl).name,
+        nickname: authors(:karl).nickname,
+        email: authors(:karl).email
+      }
+    })
+  end
+
   test "should get create" do
     get sessions_create_url
     assert_response :success
