d7c2766b5832e14e3f9b26dc335450a71fe8b246
Karl Stolley <karl.stolley@gmail.com>
2019-04-15 18:03:17 -0500

    Test login and logout with session[:uid]

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -11,6 +11,14 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_difference('Author.count') do
       # post '/auth/github/callback', params: OmniAuth.config.mock_auth[:github]
       post '/auth/github/callback', params: OmniAuth.config.add_mock(:github, {uid: '22222'})
+
+      # TODO: Figure out why in this context it's okay to access `session`:
+      assert_equal(session[:uid],'22222')
     end
+  end
+
+  test "user should be able to logout" do
+    get logout_url
+    assert_nil(session[:uid])
   end
 end
