ecc9b4f58b0921ac0811b80b86e2d4d4801162e5
Karl Stolley <karl.stolley@gmail.com>
2019-04-15 17:36:19 -0500

    Move OmniAuth setup to test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,17 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  setup do
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      uid: authors(:karl).uid,
+      info: {
+        name: authors(:karl).name,
+        nickname: authors(:karl).nickname,
+        email: authors(:karl).email
+      }
+    })
+  end
+
   # Add more helper methods to be used by all tests here...
 end
